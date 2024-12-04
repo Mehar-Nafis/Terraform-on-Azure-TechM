7980fd50d78985cd55d472d461877f458213d9b0
Mehar Nafis <108710069+Mehar-Nafis@users.noreply.github.com>
2024-12-04 15:29:23 +0530

    Update cluster.tf

--- Capstone3/cluster.tf
+++ Capstone3/cluster.tf
@@ -1,11 +1,5 @@
 resource "random_pet" "prefix" {}
 
-provider "azurerm" {
-  features {}
-  subscription_id  = "beca198a-e100-4ec3-8bff-e2ef65025f9b"
-  skip_provider_registration = true
-}
-
 resource "azurerm_resource_group" "default" {
   name     = "${random_pet.prefix.id}-rg"
   location = "Central India"
