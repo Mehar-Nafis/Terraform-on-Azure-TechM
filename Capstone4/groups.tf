504fef33c4c42ff5b6ef961f172951b34b293de4
Mehar Nafis <108710069+Mehar-Nafis@users.noreply.github.com>
2024-12-05 14:05:40 +0530

    Update groups.tf

--- Capstone4/groups.tf
+++ Capstone4/groups.tf
@@ -7,7 +7,7 @@ resource "azuread_group_member" "education" {
   for_each = { for u in azuread_user.users: u.mail_nickname => u if u.department == "Education" }
 
   group_object_id  = azuread_group.engineering.id
-  member_object_id = each.value.id
+  member_object_id = each.value.object_id
 }
 
 resource "azuread_group" "managers" {
@@ -19,7 +19,7 @@ resource "azuread_group_member" "managers" {
   for_each = { for u in azuread_user.users: u.mail_nickname => u if u.job_title == "Manager" }
 
   group_object_id  = azuread_group.managers.id
-  member_object_id = each.value.id
+  member_object_id = each.value.object_id
 }
 
 resource "azuread_group" "engineers" {
@@ -31,5 +31,5 @@ resource "azuread_group_member" "engineers" {
   for_each = { for u in azuread_user.users: u.mail_nickname => u if u.job_title == "Engineer" }
 
   group_object_id  = azuread_group.engineers.id
-  member_object_id = each.value.id
+  member_object_id = each.value.object_id
 }
